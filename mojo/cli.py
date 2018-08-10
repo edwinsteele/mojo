c964b38ada2ca26db0bc6704521ff2858a231a0c
Edwin Steele <edwin@wordspeak.org>
2018-08-10 16:38:13 +1000

    One day retrieval

--- mojo/cli.py
+++ mojo/cli.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
 
 """Console script for mojo."""
+import os
 import sys
 import click
+import requests
+
+
+
+def get_day_usage_by_hour(ymd, bearer_token):
+    headers = {
+        "Authorization": "Bearer %s" % (bearer_token,),
+        "Origin": "https://portal.mojopower.com.au",
+        "Host": "api.mojopower.com.au",
+        "Origin": "https://portal.mojopower.com.au",
+        "Referer": "https://portal.mojopower.com.au/energy",
+    }
+    url = "https://api.mojopower.com.au/app_prod/energy"
+    payload = {
+        "resolution": "hourly",
+        "startDate": "2018-08-08"
+    }
+    r = requests.get(url, params=payload, headers=headers)
+    return r.json()
 
 
 @click.command()
-def main(args=None):
+@click.option("--bearer-token",
+              prompt=True,
+              default=lambda: os.environ.get("MOJO_BEARER_TOKEN", ""),
+              help="mojopower bearer")
+def main(bearer_token):
     """Console script for mojo."""
-    click.echo("Replace this message by putting your code into "
-               "mojo.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    print(get_day_usage_by_hour("2018-08-08", bearer_token))
     return 0
 
 
